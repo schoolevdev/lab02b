b943403585b43ce357196191264626e64600f94d
schoolevdev <loddere14207@stxavier.org>
2022-09-15 14:05:13 -0400

    done

--- main.py
+++ main.py
@@ -3,19 +3,19 @@
 # Lab02b PI Approximation program.
 # This version approximates PI with a 6-sided polygon.
 #
-import math
+from math import radians, sin, pi as mpi, degrees
 #
 # Initialize Variables
-sides = 6
-AB = 1.0
-AD = 2.0
-#
+sides: int = 12
+diameter: int = 2
+interior_angle: float = 360 / sides
+# get individual length of side using sine
+side_length: float = sin(radians(interior_angle / 2))
 # Compute PI for 6-Sided Polygon
-perimeter = AB * sides
-pi = perimeter / AD
+pi = side_length * sides
 #
 # Program Output
 print("PI Approximation Program Results")
 print("Number of sides  ",sides)
 print("PI Approximation ",pi)
-print("Python's Math.pi  ",math.pi)
+print("Python's Math.pi  ",mpi)
